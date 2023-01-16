324e9025ddf9e6ad9e919fece2ed543168c9d900
Yi Lin <qinsoon@gmail.com>
2023-01-16 17:15:07 +1100

    Build debug and release separately. Add timeout

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -2,30 +2,32 @@ set -xe
 
 . $(dirname "$0")/common.sh
 
+# Need a build_type argument
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+# debug or release
+build_type=$1
+
 # helloworld.jl
 HELLO_WORLD_JL=$BINDING_PATH/.github/scripts/hello_world.jl
 
-# Do a debug build
-
 # build MMTk
-cd $MMTK_JULIA_DIR
-cargo build --features immix
-# build Julia
-cd $JULIA_PATH
-cp $BINDING_PATH/.github/scripts/Make.user $JULIA_PATH/
-MMTK_BUILD=debug make
-# Run hello world
-$JULIA_PATH/julia $HELLO_WORLD_JL
+build_args=""
+if [ "$build_type" == "release" ]; then
+    build_args=$build_args" --release"
+fi
 
-# Do a release build again
+cd $MMTK_JULIA_DIR
+cargo build --features immix $build_args
+
+cd $JULIA_PATH
+# Clean first
 make cleanall
-
-# build MMTk
-cd $MMTK_JULIA_DIR
-cargo build --features immix --release
-# build Julia
-cd $JULIA_PATH
+# Build
 cp $BINDING_PATH/.github/scripts/Make.user $JULIA_PATH/
-MMTK_BUILD=release make
+MMTK_BUILD=$build_type make
 # Run hello world
 $JULIA_PATH/julia $HELLO_WORLD_JL
