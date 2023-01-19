bed783f61074fabc556c28ace4b1af09715e03d0
Yi Lin <qinsoon@gmail.com>
2023-01-19 00:53:39 +0000

    Use make test-2 instead

--- .github/scripts/ci-test.sh
+++ .github/scripts/ci-test.sh
@@ -3,4 +3,11 @@ set -xe
 . $(dirname "$0")/common.sh
 
 # Test with 1 worker
-. $(dirname "$0")/ci-test-subset.sh 0 1
+# . $(dirname "$0")/ci-test-subset.sh 0 1
+
+# Patch some tests to skip
+. $(dirname "$0")/ci-test-patching.sh
+
+cd $JULIA_PATH
+export JULIA_TEST_MAXRSS_MB=3000
+make test-2
