b00aaa4fa2bedd844a04a7ad47418381bae1cfe8
Yi Lin <qinsoon@gmail.com>
2023-01-24 04:13:13 +0000

    Rename test-subset to test-other

--- .github/scripts/ci-test-other.sh
+++ .github/scripts/ci-test-other.sh
@@ -2,22 +2,6 @@ set -xe
 
 . $(dirname "$0")/common.sh
 
-# Need an index argument
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-    exit 1
-fi
-
-# We have an ordinal and a total number of runners. For example,
-# we have ordinal=0, and a total of 2 runners. We only run tests with
-# an even number (0, 2, 4, ...), and the other runner will run the
-# odd numbered tests.
-# To simply run all the tests, you can use ordinal=0 total=1
-ordinal=$1
-total=$2
-echo "Run tests for #${ordinal} of ${total}"
-
 # Patch some tests to skip
 . $(dirname "$0")/ci-test-patching.sh
 
@@ -29,13 +13,9 @@ REGEX_PATTERN='.*const TESTNAMES = \[(.*)^\].*'
 
 if [[ $CHOOSE_TESTS_JL_CONTENT =~ $REGEX_PATTERN ]]; then
     RAW_TEST_NAMES=${BASH_REMATCH[1]}
-    # echo "matched: $RAW_TEST_NAMES"
 
     readarray -td, test_names <<< "$RAW_TEST_NAMES"
     declare test_names
-
-    # the current index of test
-    n=0
 
     for i in "${test_names[@]}"
     do
@@ -47,18 +27,12 @@ if [[ $CHOOSE_TESTS_JL_CONTENT =~ $REGEX_PATTERN ]]; then
             # Should we skip some tests?
             # Ignore stdlib tests for now -- we run stdlib tests separately
             if [[ $test =~ "stdlib" ]]; then
-                echo "-> Skip test"
+                echo "-> Skip stdlib"
                 continue
             fi
 
-            echo "-> (Test #$n for $ordinal/$total)"
-            if [ $(( n % total )) -eq $ordinal ]; then
-                echo "-> Run"
-                ci_run_jl_test $test
-            else
-                echo "-> Skip"
-            fi
-            n=`expr $n + 1`
+            echo "-> Run"
+            ci_run_jl_test $test
         fi
     done
 else
--- .github/scripts/ci-test.sh
+++ .github/scripts/ci-test.sh
@@ -2,5 +2,5 @@ set -xe
 
 . $(dirname "$0")/common.sh
 
-. $(dirname "$0")/ci-test-subset.sh 0 1
+. $(dirname "$0")/ci-test-other.sh
 . $(dirname "$0")/ci-test-stdlib.sh
