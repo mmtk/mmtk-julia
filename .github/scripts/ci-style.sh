ccb3c6cb886670837bd0caed90ac041c1cc99360
Yi Lin <qinsoon@gmail.com>
2023-02-07 00:25:47 +0000

    Add ci-style.sh, update ci-test.sh. Test with mmtk-core

---
+++ .github/scripts/ci-style.sh
@@ -0,0 +1,12 @@
+set -xe
+
+. $(dirname "$0")/common.sh
+
+export RUSTFLAGS="-D warnings"
+
+pushd $BINDING_PATH/mmtk
+cargo clippy
+cargo clippy --release
+
+cargo fmt -- --check
+popd
--- .github/scripts/ci-test.sh
+++ .github/scripts/ci-test.sh
@@ -2,5 +2,10 @@ set -xe
 
 . $(dirname "$0")/common.sh
 
+# Build deubg
+. $(dirname "$0")/ci-build.sh debug
+# Build release
+. $(dirname "$0")/ci-build.sh release
+# Use release build to run tests
 . $(dirname "$0")/ci-test-other.sh
 . $(dirname "$0")/ci-test-stdlib.sh
