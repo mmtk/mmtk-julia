68e6ae221846785f9595c99c1751ba2782dbc38d
Yi Lin <qinsoon@gmail.com>
2023-02-07 01:11:03 +0000

    Disable clippy check. Fix typo

--- .github/scripts/ci-style.sh
+++ .github/scripts/ci-style.sh
@@ -5,8 +5,12 @@ set -xe
 export RUSTFLAGS="-D warnings"
 
 pushd $BINDING_PATH/mmtk
-cargo clippy
-cargo clippy --release
+
+# Currently we have many warnings from clippy, and we have PRs sitting there to be merged.
+# I am concerned that enabling this will cause merge conflicts everywhere.
+# However, we should enable this as soon as we can.
+# cargo clippy
+# cargo clippy --release
 
 cargo fmt -- --check
 popd
