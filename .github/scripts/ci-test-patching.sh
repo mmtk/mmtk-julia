678a4363271cb638b0decad4530c9a49de76061e
Yi Lin <qinsoon@gmail.com>
2023-09-01 00:32:55 +0000

    Ignore a few new tests about gc threads

--- .github/scripts/ci-test-patching.sh
+++ .github/scripts/ci-test-patching.sh
@@ -17,7 +17,9 @@ declare -a tests_to_skip=(
     # These tests check for the number of stock GC threads (which we set to 0 with mmtk)
     '@test (cpu_threads == 1 ? "1" : string(div(cpu_threads, 2))) ==' "$JULIA_PATH/test/cmdlineargs.jl"
     '@test read(`$exename --gcthreads=2 -e $code`, String) == "2"' "$JULIA_PATH/test/cmdlineargs.jl"
+    '@test read(`$exename --gcthreads=2,1 -e $code`, String) == "3"' "$JULIA_PATH/test/cmdlineargs.jl"
     '@test read(`$exename -e $code`, String) == "2"' "$JULIA_PATH/test/cmdlineargs.jl"
+    '@test read(`$exename -e $code`, String) == "3"' "$JULIA_PATH/test/cmdlineargs.jl"
 
     # This test checks GC logging
     '@test occursin("GC: pause", read(tmppath, String))' "$JULIA_PATH/test/misc.jl"
