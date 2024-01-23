d97228fb81d2e5c602098a1968f4a94962979436
Yi Lin <qinsoon@gmail.com>
2024-01-23 15:57:08 +1300

    Run CI and mergify for v1.9.2+RAI (#112)
    This PR allows CI and mergify to work with `v1.9.2+RAI` and `v1.8.2+RAI`
    (renamed from `v1.8.2-RAI`). It also changes the CI script to set the
    test max RSS based on the system memory (it seems Github hosted runners
    had an upgrade, and they now have 16G memory instead of 7G).

--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -10,8 +10,9 @@ export MMTK_JULIA_DIR=$BINDING_PATH
 # Make sure we have enough heap to build Julia
 export MMTK_MIN_HSIZE_G=0.5
 export MMTK_MAX_HSIZE_G=4
-# Make sure we do not get OOM killed. The Github runner has ~7G RAM.
-export JULIA_TEST_MAXRSS_MB=6500
+# Make sure we do not get OOM killed.
+total_mem=$(free -m | awk '/^Mem:/ {print $2}')
+export JULIA_TEST_MAXRSS_MB=$total_mem
 
 ci_run_jl_test() {
     test=$1
