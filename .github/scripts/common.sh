c40a15f002b5e0acacbbc1ab75d6538658e151dc
Eduardo Souza <ledusou@gmail.com>
2024-03-19 04:06:00 +0000

    Adding RUST_BACKTRACE=1 to try to capture errors that happen in the runner

--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -10,6 +10,8 @@ export MMTK_JULIA_DIR=$BINDING_PATH
 # Make sure we have enough heap to build Julia
 export MMTK_MIN_HSIZE_G=0.5
 export MMTK_MAX_HSIZE_G=4
+# Print out a backtrace in case of an error
+export RUST_BACKTRACE=1
 # Make sure we do not get OOM killed.
 total_mem=$(free -m | awk '/^Mem:/ {print $2}')
 export JULIA_TEST_MAXRSS_MB=$total_mem
