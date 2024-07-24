6a287364e68db5d6580a724871f535ddd8a4a7d6
Yi Lin <qinsoon@gmail.com>
2024-07-24 17:41:42 +1200

    Do not tpin roorts for non moving builds (#162)

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -26,6 +26,11 @@ fi
 
 plan_feature=${plan,,}
 moving_feature=${is_moving,,}
+if [ "$is_moving" == "Default" ]; then
+    tpin_roots=1
+else
+    tpin_roots=0
+fi
 
 cd $MMTK_JULIA_DIR/mmtk
 cargo build --features $plan_feature,$moving_feature $build_args
@@ -36,6 +41,6 @@ cd $JULIA_PATH
 make cleanall
 # Build
 cp $BINDING_PATH/.github/scripts/Make.user $JULIA_PATH/
-MMTK_PLAN=$plan MMTK_BUILD=$build_type make
+MMTK_PLAN=$plan MMTK_BUILD=$build_type MMTK_TPIN_ROOTS=$tpin_roots make
 # Run hello world
 $JULIA_PATH/julia $HELLO_WORLD_JL
