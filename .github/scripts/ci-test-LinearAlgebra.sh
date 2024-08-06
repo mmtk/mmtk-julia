ffea9770f7d24d1f156f755ac37eab06bc917c19
Luis Eduardo de Souza Amorim <eduardo@groundhog.moma>
2024-08-06 05:56:30 +0000

    Trying it with 2 workers and 6Gb each

--- .github/scripts/ci-test-LinearAlgebra.sh
+++ .github/scripts/ci-test-LinearAlgebra.sh
@@ -5,10 +5,10 @@ set -e
 
 . $(dirname "$0")/common.sh
 
-export MMTK_MAX_HSIZE_G=4
+export MMTK_MAX_HSIZE_G=6
 total_mem=$(free -m | awk '/^Mem:/ {print $2}')
-num_workers=3
+num_workers=2
 export JULIA_TEST_MAXRSS_MB=$((total_mem/ num_workers))
 
 echo "-> Run single threaded"
-ci_run_jl_test "LinearAlgebra" 3
+ci_run_jl_test "LinearAlgebra" 2
