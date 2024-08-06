07089e6fb6975323ecf9188d00964105ca7a72c5
Luis Eduardo de Souza Amorim <eduardo@groundhog.moma>
2024-08-06 04:02:30 +0000

    Ensuring we do not get OOM killed.

--- .github/scripts/ci-test-LinearAlgebra.sh
+++ .github/scripts/ci-test-LinearAlgebra.sh
@@ -6,6 +6,9 @@ set -e
 . $(dirname "$0")/common.sh
 
 export MMTK_MAX_HSIZE_G=4
+total_mem=$(free -m | awk '/^Mem:/ {print $2}')
+num_workers=3
+export JULIA_TEST_MAXRSS_MB=$((total_mem/ num_workers))
 
 echo "-> Run single threaded"
 ci_run_jl_test "LinearAlgebra" 3
