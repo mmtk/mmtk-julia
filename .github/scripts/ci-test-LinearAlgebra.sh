31034a68096212ac49673c4d84c989a8fa2b062c
Luis Eduardo de Souza Amorim <eduardo@groundhog.moma>
2024-08-06 08:26:28 +0000

    Setting max rss and keep nworkers = 1

--- .github/scripts/ci-test-LinearAlgebra.sh
+++ .github/scripts/ci-test-LinearAlgebra.sh
@@ -5,10 +5,10 @@ set -e
 
 . $(dirname "$0")/common.sh
 
-export MMTK_MAX_HSIZE_G=5
+export MMTK_MAX_HSIZE_G=8
 total_mem=$(free -m | awk '/^Mem:/ {print $2}')
-num_workers=2
+num_workers=1
 export JULIA_TEST_MAXRSS_MB=$((total_mem/ num_workers))
 
 echo "-> Run single threaded"
-ci_run_jl_test "LinearAlgebra" 2
+ci_run_jl_test "LinearAlgebra" 1
