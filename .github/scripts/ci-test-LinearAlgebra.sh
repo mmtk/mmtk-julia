a4cfaf16f72781794193347c4db749f206584939
Eduardo Souza <ledusou@gmail.com>
2024-08-07 11:19:11 +0000

    Reset LinearAlgebra tests to what it was before

--- .github/scripts/ci-test-LinearAlgebra.sh
+++ .github/scripts/ci-test-LinearAlgebra.sh
@@ -5,10 +5,5 @@ set -e
 
 . $(dirname "$0")/common.sh
 
-export MMTK_MAX_HSIZE_G=10
-total_mem=$(free -m | awk '/^Mem:/ {print $2}')
-mem_threshold=1536 # use 1.5Gb as a threshold for the max rss based on the total free memory
-export JULIA_TEST_MAXRSS_MB=$((total_mem- mem_threshold))
-
 echo "-> Run single threaded"
 ci_run_jl_test "LinearAlgebra" 1
