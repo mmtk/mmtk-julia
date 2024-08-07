f33b31bdb1bfb4c2ad806719ac5741511fde9f0d
Eduardo Souza <ledusou@gmail.com>
2024-08-07 23:47:06 +0000

    Playing around with heap size and maxrss for the linear algebra tests

--- .github/scripts/ci-test-LinearAlgebra.sh
+++ .github/scripts/ci-test-LinearAlgebra.sh
@@ -5,5 +5,12 @@ set -e
 
 . $(dirname "$0")/common.sh
 
+export MMTK_MAX_HSIZE_G=5
+total_mem=$(free -m | awk '/^Mem:/ {print $2}')
+mem_threshold=512 # use 0.5Gb as a threshold for the max rss based on the total free memory
+total_mem_restricted=$((total_mem- mem_threshold))
+num_workers=2
+export JULIA_TEST_MAXRSS_MB=$((total_mem_restricted/ num_workers))
+
 echo "-> Run single threaded"
-ci_run_jl_test "LinearAlgebra" 1
+ci_run_jl_test "LinearAlgebra" 2
