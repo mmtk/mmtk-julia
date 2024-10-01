d4e1c8d7f9915a2cbc4c00fe6ba346ad42dcbbfe
Yi Lin <qinsoon@gmail.com>
2024-10-01 00:28:26 +0000

    Run ci-test-other in two jobs, each runs half of the tests

--- .github/scripts/ci-test-other.sh
+++ .github/scripts/ci-test-other.sh
@@ -1,5 +1,14 @@
 set -xe
 
+# Parse arguments: i and n
+i=$1
+n=$2
+
+if [[ -z "$i" || -z "$n" ]]; then
+    echo "Usage: $0 <i> <n>"
+    exit 1
+fi
+
 . $(dirname "$0")/common.sh
 
 # Get all the tests
@@ -14,11 +23,33 @@ if [[ $CHOOSE_TESTS_JL_CONTENT =~ $REGEX_PATTERN ]]; then
     readarray -td, test_names <<< "$RAW_TEST_NAMES"
     declare test_names
 
-    for i in "${test_names[@]}"
+    # Calculate the total number of tests
+    total_tests=${#test_names[@]}
+    if [[ "$total_tests" -eq 0 ]]; then
+        echo "No tests found."
+        exit 1
+    fi
+
+    # Calculate start and end indices for the ith part
+    part_size=$(( (total_tests + n - 1) / n ))  # This rounds up to ensure all tests are covered
+    start_index=$(( i * part_size ))
+    end_index=$(( (i + 1) * part_size ))
+
+    if [[ "$start_index" -ge "$total_tests" ]]; then
+        echo "No tests to run for this part."
+        exit 0
+    fi
+
+    if [[ "$end_index" -gt "$total_tests" ]]; then
+        end_index=$total_tests
+    fi
+
+    # Run the ith part of the tests
+    for (( j=$start_index; j<$end_index; j++ ))
     do
-        # echo "Token: '$i'"
-        test=`sed 's/\"\(.*\)\"/\1/' <<< $i`
+        test=`sed 's/\"\(.*\)\"/\1/' <<< "${test_names[j]}"`
         if [[ ! -z "$test" ]]; then
+            echo $j
             echo $test
 
             # Should we skip some tests?
