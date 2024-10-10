db65d31b96140a2b6f39478e50493339b0435d90
Eduardo Souza <ledusou@gmail.com>
2024-10-10 04:33:08 +0000

    Skipping tests that check the reasons for a full sweep and are specific to stock Julia

--- .github/scripts/ci-test-patching.sh
+++ .github/scripts/ci-test-patching.sh
@@ -38,6 +38,10 @@ declare -a tests_to_skip=(
     '@test !live_bytes_has_grown_too_much' "$JULIA_PATH/test/gc.jl"
     '@test any(page_utilization .> 0)' "$JULIA_PATH/test/gc.jl"
 
+    # Tests that check the reasons for a full sweep and are specific to stock Julia
+    '@test reasons\[:FULL_SWEEP_REASON_FORCED_FULL_SWEEP\] >= 1' "$JULIA_PATH/test/gc.jl"
+    '@test keys(reasons) == Set(Base.FULL_SWEEP_REASONS)' "$JULIA_PATH/test/gc.jl"
+
     # Allocation profiler tests that fail when we inline fastpath allocation
     '@test length(\[a for a in prof.allocs if a.type == MyType\]) >= 1' "$JULIA_PATH/stdlib/Profile/test/allocs.jl"
     '@test length(prof.allocs) >= 1' "$JULIA_PATH/stdlib/Profile/test/allocs.jl"
