c131283c57961f418f15c6457c328b0b8bc77289
Eduardo Souza <ledusou@gmail.com>
2024-10-18 15:13:01 +1100

    Updating the binding to support MMTk with upstream Julia (#180)
    This PR updates `update-ready/immix` to support its counterpart in
    [`mmtk/julia`](https://github.com/mmtk/julia/tree/upstream-ready/immix).
    It should enable MMTk to run with the latest upstream Julia, but
    currently supporting only non-moving immix. It also cleans up the
    binding code, removing the C files and moving it into the Julia repo
    ([`gc-mmtk.c`](https://github.com/mmtk/julia/blob/upstream-ready/immix/src/gc-mmtk.c)).
    Finally, it removes the duplication that we had to use Bindgen, and
    instead generates the Rust bindings using a `build.rs` file.

--- julia/mmtk_julia.h
+++
@@ -1,8 +0,0 @@
-#include "mmtk.h"
-#include "gc.h"
-
-extern Julia_Upcalls mmtk_upcalls;
-
-int set_gc_running_state(jl_ptls_t ptls);
-void mmtk_jl_gc_run_all_finalizers(void);
-void mmtk_jl_run_pending_finalizers(void* tls);
