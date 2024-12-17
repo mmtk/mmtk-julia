ae161a08519dae8f040ed84253b01ae3954c5b6d
Eduardo Souza <ledusou@gmail.com>
2024-12-17 03:21:47 +0000

    Removing CI information

--- .github/scripts/ci-setup.sh
+++
@@ -1,9 +0,0 @@
-set -xe
-
-. $(dirname "$0")/common.sh
-
-rustup toolchain install $RUSTUP_TOOLCHAIN
-rustup target add i686-unknown-linux-gnu --toolchain $RUSTUP_TOOLCHAIN
-rustup component add clippy --toolchain $RUSTUP_TOOLCHAIN
-rustup component add rustfmt --toolchain $RUSTUP_TOOLCHAIN
-rustup override set $RUSTUP_TOOLCHAIN
