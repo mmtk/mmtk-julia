7f762771fc2bc0caa33ba0245dc18a55067493e6
Luis Eduardo de Souza Amorim <eduardo@rat.moma>
2025-01-08 01:35:30 +0000

    Minor

--- .github/scripts/ci-ffi.sh
+++ .github/scripts/ci-ffi.sh
@@ -6,7 +6,7 @@ pushd $MMTK_JULIA_DIR
 
 make regen-bindgen-ffi
 
-if ! git diff --exit-code mmtk/julia_types.rs; then
+if ! git diff --exit-code $MMTK_JULIA_DIR/mmtk/src/julia_types.rs; then
   echo "Rust FFI bindings in \`julia_types.rs\` are outdated. Run \`make regen-bindgen-ffi\` from the mmtk-julia directory and make sure to include the updated file in the pull request."
   exit 1
 fi
