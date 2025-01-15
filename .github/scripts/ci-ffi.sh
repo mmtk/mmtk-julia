ecc9f852b68b17796830344d39ab73e3c697ba77
Yi Lin <qinsoon@gmail.com>
2025-01-15 03:03:44 +0000

    Put ffi check to a different job. Add another job to run with generated ffi

--- .github/scripts/ci-ffi.sh
+++ .github/scripts/ci-ffi.sh
@@ -6,10 +6,5 @@ pushd $MMTK_JULIA_DIR
 
 make regen-bindgen-ffi
 
-if ! git diff --exit-code $MMTK_JULIA_DIR/mmtk/src/julia_types.rs; then
-  echo "Rust FFI bindings in \`julia_types.rs\` are outdated. Run \`make regen-bindgen-ffi\` from the mmtk-julia directory and make sure to include the updated file in the pull request."
-  exit 1
-fi
-
 popd
 
