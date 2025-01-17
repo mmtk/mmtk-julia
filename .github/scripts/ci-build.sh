9b59bb5822322a66359d2bf0387d2c77f974c782
Eduardo Souza <ledusou@gmail.com>
2025-01-18 06:19:51 +1100

    Adding print versioninfo and cleaning up build script for CI (#218)

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -33,16 +33,12 @@ else
     MOVING=0
 fi
 
-cd $MMTK_JULIA_DIR
-
-MMTK_MOVING=$MOVING make $build_type
-
 cd $JULIA_PATH
-
 # Clean first
 make cleanall
-# Build
+# This will build the binding in MMTK_JULIA_DIR (defined in common.sh), and link it
+# when building Julia, instead of using the set version defined in Julia itself 
 cp $BINDING_PATH/.github/scripts/Make.user $JULIA_PATH/
-MMTK_PLAN=$plan MMTK_BUILD=$build_type make
+MMTK_MOVING=$MOVING MMTK_PLAN=$plan MMTK_BUILD=$build_type make
 # Run hello world
 $JULIA_PATH/julia $HELLO_WORLD_JL
