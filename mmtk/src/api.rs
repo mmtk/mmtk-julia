000e78ccd82fb1ef53c892c22cc6977a36c11c89
Diogo Netto <61364108+d-netto@users.noreply.github.com>
2025-06-23 20:03:29 -0300

    Fix possible typo in mmtk_is_pointer_pinned (#250)
    Seems like a typo: it looks like the `if else` branch should be handling
    objects that are managed by MMTk, but that are not in IMMIX space.

--- mmtk/src/api.rs
+++ mmtk/src/api.rs
@@ -612,7 +612,7 @@ pub extern "C" fn mmtk_is_pointer_pinned(addr: Address) -> bool {
 
     if crate::object_model::is_addr_in_immixspace(addr) {
         handle_potential_internal_pointer!(memory_manager::is_pinned, addr)
-    } else if !mmtk_object_is_managed_by_mmtk(addr.as_usize()) {
+    } else if mmtk_object_is_managed_by_mmtk(addr.as_usize()) {
         debug!(
             "Object is not in Immix space. MMTk will not move the object. We assume it is pinned."
         );
