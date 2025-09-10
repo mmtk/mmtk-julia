8a92b0247bd1734c4b6cbf9607bbf6f3be3c2f59
Eduardo Souza <ledusou@gmail.com>
2025-09-10 06:17:25 +0000

    Cleanup

--- .github/scripts/ci-test-stdlib.sh
+++ .github/scripts/ci-test-stdlib.sh
@@ -34,10 +34,7 @@ declare -a tests_to_skip=(
 declare -a max_moving_tests_to_skip=(
     # Skipping tests that fail for max moving Immix
     # see https://github.com/mmtk/mmtk-julia/issues/259
-    # "Downloads"
-    # "REPL"
     "Random"
-    # "LibCURL"
     "LazyArtifacts"
 )
 
