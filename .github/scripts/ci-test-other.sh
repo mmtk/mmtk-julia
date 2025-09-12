22ff3dc50802f6c186194a6c8aa0074522e095bd
Eduardo Souza <ledusou@gmail.com>
2025-09-12 10:04:12 +1000

    Enabling `cmdlineargs` and `loading` tests (#273)

--- .github/scripts/ci-test-other.sh
+++ .github/scripts/ci-test-other.sh
@@ -20,8 +20,6 @@ moving_feature=${is_moving,,}
 declare -a max_moving_tests_to_skip=(
     # see https://github.com/mmtk/mmtk-julia/issues/259
     "abstractarray"
-    "cmdlineargs"
-    "loading"
     "misc"
 )
 
